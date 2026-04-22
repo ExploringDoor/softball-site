c2d949d3131dbde443c10050e2d745bf40749330
exploring <adam.miller.22@gmail.com>
2026-04-21 23:49:24 -0400

    v96: bump service worker for bug-audit fixes
    Forces all clients to pick up the v96 correctness fixes on next visit.

--- sw.js
+++ sw.js
@@ -165,7 +165,7 @@ self.addEventListener('notificationclick', function(event) {
   })());
 });
 
-const VERSION = `${LEAGUE.id}-v95-monorepo-config`;
+const VERSION = `${LEAGUE.id}-v96-bug-audit-fixes`;
 const CORE_CACHE = `${LEAGUE.id}-core-${VERSION}`;
 const RUNTIME_CACHE = `${LEAGUE.id}-runtime-${VERSION}`;
 
