31de55a1899dc6189d9a2b1e9611182061624653
exploring <adam.miller.22@gmail.com>
2026-06-07 20:23:25 -0400

    Revert "Add softball emoji 🥎 favicon to every page (v320)"
    This reverts commit 234fb072d1a4f2e2662cd154a7eba0e493a6d246.

--- sw.js
+++ sw.js
@@ -174,7 +174,7 @@ self.addEventListener('notificationclick', function(event) {
   })());
 });
 
-const VERSION = `${LEAGUE.id}-v320-softball-emoji-favicon`;
+const VERSION = `${LEAGUE.id}-v319-ticker-skips-past-dated-not-done-weeks`;
 const CORE_CACHE = `${LEAGUE.id}-core-${VERSION}`;
 const RUNTIME_CACHE = `${LEAGUE.id}-runtime-${VERSION}`;
 
